49d4bec222c6b066a25e8b72d43dc08c07b9319c
Gregory Lee <grlee77@gmail.com>
2018-12-06 02:17:54 -0500

    add the driver routine cuFuncGetAttribute
    add a FuncAttributes class to store all kernel attributes
    
    add an attributes property to the RawKernel class. This can be used to query the register
    and memory usage of the kernel.

--- cupy/cuda/cupy_cuda.h
+++ cupy/cuda/cupy_cuda.h
@@ -52,6 +52,7 @@ typedef enum {
 } CUresult;
 enum CUjit_option {};
 enum CUjitInputType {};
+enum CUfunction_attribute {};
 
 
 typedef void* CUdeviceptr;
@@ -143,6 +144,10 @@ CUresult cuLaunchKernel(...) {
     return CUDA_SUCCESS;
 }
 
+// Function attribute
+CUresult cuFuncGetAttribute(...) {
+    return CUDA_SUCCESS;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // cuda_runtime.h
