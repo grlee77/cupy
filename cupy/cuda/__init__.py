db75ce5e539791d6db0df2ae1075df61a09faab7
Gregory Lee <grlee77@gmail.com>
2018-12-06 00:39:59 -0500

    add get_attributes_dict for building a dict of CUDA device attributes

--- cupy/cuda/__init__.py
+++ cupy/cuda/__init__.py
@@ -69,6 +69,19 @@ def get_cuda_path():
     return _cuda_path
 
 
+def get_attributes_dict(device_id):
+    """Return a dict containing all device attributes."""
+    d = {}
+    for k, v in runtime.__dict__.items():
+        if 'CU_DEVICE_ATTRIBUTE' in k:
+            try:
+                name = k.replace('CU_DEVICE_ATTRIBUTE_', '')
+                d[name] = runtime.deviceGetAttribute(v, device_id)
+            except runtime.CUDARuntimeError:
+                pass
+    return d
+
+
 # import class and function
 from cupy.cuda.compiler import compile_with_cache  # NOQA
 from cupy.cuda.device import Device  # NOQA
