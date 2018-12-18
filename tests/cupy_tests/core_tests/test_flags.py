45d88d3e83caa04e9046ca47f8eb075337c839e8
Gregory R. Lee <gregory.lee@cchmc.org>
2018-12-18 15:49:16 -0500

    add fnc and forc to Flags
    forc is True if the array is either Fortran or C contiguous
    fnc is True if the array is Fortran contiguous and not C contiguous

--- tests/cupy_tests/core_tests/test_flags.py
+++ tests/cupy_tests/core_tests/test_flags.py
@@ -1,6 +1,8 @@
 import unittest
 
+import cupy
 from cupy.core import flags
+from cupy import testing
 
 
 class TestFlags(unittest.TestCase):
@@ -25,3 +27,23 @@ class TestFlags(unittest.TestCase):
         self.assertEqual('''  C_CONTIGUOUS : 1
   F_CONTIGUOUS : 2
   OWNDATA : 3''', repr(self.flags))
+
+    def test_fnc(self):
+        a = testing.shaped_random((4, ), cupy)
+        a_2d_c = testing.shaped_random((4, 4), cupy)
+        a_2d_f = cupy.asfortranarray(testing.shaped_random((4, 4), cupy))
+        a_noncontig = testing.shaped_random((4, 8), cupy)[:, ::2]
+        assert a.flags.fnc == False
+        assert a_2d_c.flags.fnc == False
+        assert a_2d_f.flags.fnc == True
+        assert a_noncontig.flags.fnc == False
+
+    def test_forc(self):
+        a = testing.shaped_random((4, ), cupy)
+        a_2d_c = testing.shaped_random((4, 4), cupy)
+        a_2d_f = cupy.asfortranarray(testing.shaped_random((4, 4), cupy))
+        a_noncontig = testing.shaped_random((4, 8), cupy)[:, ::2]
+        assert a.flags.forc == True
+        assert a_2d_c.flags.forc == True
+        assert a_2d_f.flags.forc == True
+        assert a_noncontig.flags.forc == False
