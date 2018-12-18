9debe911117e4b3c8acf921542a1705b175548f8
Gregory R. Lee <gregory.lee@cchmc.org>
2018-12-18 16:08:49 -0500

    update testing for Flags
    Conflicts:
    	tests/cupy_tests/core_tests/test_flags.py

--- tests/cupy_tests/core_tests/test_flags.py
+++ tests/cupy_tests/core_tests/test_flags.py
@@ -28,22 +28,46 @@ class TestFlags(unittest.TestCase):
   F_CONTIGUOUS : 2
   OWNDATA : 3''', repr(self.flags))
 
-    def test_fnc(self):
-        a = testing.shaped_random((4, ), cupy)
-        a_2d_c = testing.shaped_random((4, 4), cupy)
-        a_2d_f = cupy.asfortranarray(testing.shaped_random((4, 4), cupy))
-        a_noncontig = testing.shaped_random((4, 8), cupy)[:, ::2]
-        assert a.flags.fnc == False
-        assert a_2d_c.flags.fnc == False
-        assert a_2d_f.flags.fnc == True
-        assert a_noncontig.flags.fnc == False
 
-    def test_forc(self):
-        a = testing.shaped_random((4, ), cupy)
-        a_2d_c = testing.shaped_random((4, 4), cupy)
-        a_2d_f = cupy.asfortranarray(testing.shaped_random((4, 4), cupy))
-        a_noncontig = testing.shaped_random((4, 8), cupy)[:, ::2]
-        assert a.flags.forc == True
-        assert a_2d_c.flags.forc == True
-        assert a_2d_f.flags.forc == True
-        assert a_noncontig.flags.forc == False
+@testing.parameterize(
+    *testing.product({
+        'case': ['1d', '1d_noncontig', '2d_c', '2d_f', '2d_noncontig'],
+    })
+)
+class TestContiguityFlags(unittest.TestCase):
+
+    def setUp(self):
+        self.flags = None
+
+    def init_flags(self, xp):
+        if self.case == '1d':
+            a = testing.shaped_arange((4, ), xp)
+        elif self.case == '1d_noncontig':
+            a = testing.shaped_arange((8, ), xp)[::2]
+        elif self.case == '2d_c':
+            a = testing.shaped_arange((4, 4), xp)
+        elif self.case == '2d_f':
+            a = xp.asfortranarray(testing.shaped_arange((4, 4), xp))
+        elif self.case == '2d_noncontig':
+            a = testing.shaped_arange((4, 8), xp)[:, ::2]
+        self.flags = a.flags
+
+    @testing.numpy_cupy_equal()
+    def test_fnc(self, xp):
+        self.init_flags(xp)
+        return self.flags.fnc
+
+    @testing.numpy_cupy_equal()
+    def test_forc(self, xp):
+        self.init_flags(xp)
+        return self.flags.forc
+
+    @testing.numpy_cupy_equal()
+    def test_f_contiguous(self, xp):
+        self.init_flags(xp)
+        return self.flags.f_contiguous
+
+    @testing.numpy_cupy_equal()
+    def test_c_contiguous(self, xp):
+        self.init_flags(xp)
+        return self.flags.c_contiguous
