b7a36d60d4ce128bec8fb74afda299effff8c88e
Gregory Lee <grlee77@gmail.com>
2018-12-19 03:38:18 -0500

    fix unique for size 0 inputs
    unique should return a size 0 array for size 0 input

--- cupy/manipulation/add_remove.py
+++ cupy/manipulation/add_remove.py
@@ -68,8 +68,9 @@ def unique(ar, return_index=False, return_inverse=False,
         ar.sort()
         aux = ar
     mask = cupy.empty(aux.shape, dtype=cupy.bool_)
-    mask[0] = True
-    mask[1:] = aux[1:] != aux[:-1]
+    if mask.size > 0:
+        mask[0] = True
+        mask[1:] = aux[1:] != aux[:-1]
 
     ret = aux[mask]
     if not return_index and not return_inverse and not return_counts:
