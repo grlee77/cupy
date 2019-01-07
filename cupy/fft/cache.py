dea81897633630e34cb5460952821fca0fdc8dd9
Gregory R. Lee <gregory.lee@cchmc.org>
2019-01-07 12:29:17 -0500

    modify cache for use with CuPy instead of PyFFTW

--- cupy/fft/cache.py
+++ cupy/fft/cache.py
@@ -5,6 +5,11 @@
 # Henry Gomersall
 # heng@kedevelopments.co.uk
 #
+# Copyright 2018 (modifications for CuPy)
+#
+# Gregory R. Lee
+# grlee77@gmail.com
+#
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -34,92 +39,93 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-'''
-During calls to functions implemented in :mod:`pyfftw.interfaces`, a
-:class:`pyfftw.FFTW` object is necessarily created. Although the time to
-create a new :class:`pyfftw.FFTW` is short (assuming that the planner
-possesses the necessary wisdom to create the plan immediately), it may
-still take longer than a short transform.
+"""
+During calls to functions :mod:`cupy.fftn` and `cupy.ifftn`, a
+CUFFT plan object is necessarily created. Although the time to create a new
+plan is typically short, it may still take longer than a short transform.
 
-This module implements a method by which objects that are created through
-:mod:`pyfftw.interfaces` are temporarily cached. If an equivalent
-transform is then performed within a short period, the object is acquired
-from the cache rather than a new one created. The equivalency is quite
+This module implements a method by which CUFFT plans are temporarily cached. If
+an equivalent transform is then performed within a short period, the object is
+acquired from the cache rather than a new one created. The equivalency is quite
 conservative and in practice means that if any of the arguments change, or
 if the properties of the array (shape, strides, dtype) change in any way, then
 the cache lookup will fail.
 
-The cache temporarily stores a copy of any interim :class:`pyfftw.FFTW`
-objects that are created. If they are not used for some period of time,
-which can be set with :func:`pyfftw.interfaces.cache.set_keepalive_time`,
-then they are removed from the cache (liberating any associated memory).
-The default keepalive time is 0.1 seconds.
+The cache temporarily stores a copy of any interim `cupy.cuda.cufft.PlanNd` or
+`cupy.cuda.cufft.Plan1d` objects that are created. If they are not used for
+some period of time, which can be set with
+:func:`cupy.fft.cache.set_keepalive_time`, then they are removed from the cache
+(liberating any associated memory).
+The default keepalive time is 1.0 seconds.
 
-Enable the cache by calling :func:`pyfftw.interfaces.cache.enable`.
-Disable it by calling :func:`pyfftw.interfaces.cache.disable`. By default,
-the cache is disabled.
+Enable the cache by calling :func:`cupy.fft.cache.enable`.
+Disable it by calling :func:`cupy.fft.cache.disable`. By default, the cache is
+disabled.
 
 Note that even with the cache enabled, there is a fixed overhead associated
 with lookups. This means that for small transforms, the overhead may exceed
-the transform. At this point, it's worth looking at using :class:`pyfftw.FFTW`
-directly.
+the transform duration.
 
 When the cache is enabled, the module spawns a new thread to keep track
-of the objects. If :mod:`threading` is not available, then the cache
-is not available and trying to use it will raise an ImportError exception.
+of the objects. If :mod:`threading` is not available, then the cache is not
+available and trying to use it will raise an ImportError exception.
 
-The actual implementation of the cache is liable to change, but the
-documented API is stable.
-'''
+The actual implementation of the cache is liable to change, but the documented
+API is stable.
+"""
 
 try:
     import threading as _threading
     _threading_import_error = None
 except ImportError as e:
     _threading_import_error = e
-    _threading == None
+    _threading = None
 
 import time
 import weakref
 
 __all__ = ['enable', 'disable', 'set_keepalive_time']
 
-_fftw_cache = None
+_cufft_cache = None
+
 
 class CacheError(Exception):
     pass
 
+
 def enable():
-    '''Enable the cache.
-    '''
-    global _fftw_cache
+    """Enable the cache."""
+    global _cufft_cache
 
     if _threading is not None:
-        if _fftw_cache is None:
-            _fftw_cache = _Cache()
+        if _cufft_cache is None:
+            _cufft_cache = _Cache()
     else:
         raise ImportError(_threading_import_error)
 
+
 def disable():
-    '''Disable the cache.
-    '''
-    global _fftw_cache
-    _fftw_cache = None
+    """Disable the cache."""
+    global _cufft_cache
+    _cufft_cache = None
+
 
 def is_enabled():
-    '''Return whether the cache is currently enabled.
-    '''
-    if _fftw_cache is None:
+    """Return whether the cache is currently enabled."""
+    if _cufft_cache is None:
         return False
     else:
         return True
 
+
 def set_keepalive_time(keepalive_time):
-    '''Set the minimum time in seconds for which any :mod:`pyfftw.FFTW` object
+    """Set the minimum time in seconds for cached CUFFT plans.
+
+    Which any :mod:`cupy.cuda.cufft.PlanNd` or `cupy.cuda.cufft.Plan1d`object
     in the cache is kept alive.
 
     When the cache is enabled, the interim objects that are used through
-    a :mod:`pyfftw.interfaces` function are cached for the time set through
+    a functions in the `cupy.fft` module are cached for the time set through
     this function. If the object is not used for the that time, it is
     removed from the cache. Using the object zeros the timer.
 
@@ -127,13 +133,14 @@ def set_keepalive_time(keepalive_time):
     practice, it may be quite a bit longer before the object is
     deleted from the cache (due to implementational details - e.g. contention
     from other threads).
-    '''
-    global _fftw_cache
+    """
+    global _cufft_cache
 
-    if _fftw_cache is None:
+    if _cufft_cache is None:
         raise CacheError('Cache is not currently enabled')
     else:
-        _fftw_cache.set_keepalive_time(keepalive_time)
+        _cufft_cache.set_keepalive_time(keepalive_time)
+
 
 class _Cache(object):
 
@@ -141,7 +148,7 @@ class _Cache(object):
     def keepalive_time(self):
         return self._keepalive_time
 
-    def __init__(self, keepalive_time=0.1):
+    def __init__(self, keepalive_time=1.0):
 
         self._cache_dict = {}
         self.set_keepalive_time(keepalive_time)
@@ -158,10 +165,11 @@ class _Cache(object):
         self._close_thread_now = _threading.Event()
 
         self._initialised = _threading.Event()
-        self._initialised.clear() # Explicitly clear it for clarity
+        self._initialised.clear()  # Explicitly clear it for clarity
 
         self._thread_object = _threading.Thread(target=_Cache._run,
-                args=(weakref.proxy(self), ), name='PyFFTWCacheThread')
+                                                args=(weakref.proxy(self), ),
+                                                name='CuPyCUFFTCacheThread')
 
         self._thread_object.daemon = True
         self._thread_object.start()
@@ -195,7 +203,7 @@ class _Cache(object):
 
             while True:
                 if (not self._parent_thread.is_alive() or
-                    self._close_thread_now.is_set()):
+                        self._close_thread_now.is_set()):
                     break
 
                 if time.time() - last_cull_time > self._keepalive_time:
@@ -210,7 +218,7 @@ class _Cache(object):
                         with self._keepalive_set_lock:
                             # Work out which should be culled
                             cull_set = set(new_cache_dict).difference(
-                                    self._keepalive_set)
+                                self._keepalive_set)
 
                             self._keepalive_set = set()
 
@@ -226,39 +234,41 @@ class _Cache(object):
         except ReferenceError:
             pass
 
-    def set_keepalive_time(self, keepalive_time=0.1):
-        '''Set the minimum time in seconds for which any object in the cache
-        is kept alive.
+    def set_keepalive_time(self, keepalive_time=1.0):
+        """Set the minimum time in seconds.
+
+        This is the minimum time for which any object in the cache is kept
+        alive.
 
         The time is not precise, and sets a minimum time to be alive. In
         practice, it may be up to twice as long before the object is
         deleted from the cache (due to implementational details).
-        '''
+        """
         self._keepalive_time = float(keepalive_time)
 
-        if self._keepalive_time/2 > 0.1:
+        if self._keepalive_time / 2 > 0.1:
             self._wakeup_time = 0.1
         else:
-            self._wakeup_time = self._keepalive_time/2
+            self._wakeup_time = self._keepalive_time / 2
 
     def _refresh(self, key):
-        '''Refresh the object referenced by key to stop it being culled
+        """Refresh the object referenced by key to stop it being culled
         on the next round.
-        '''
+        """
         with self._keepalive_set_lock:
             self._keepalive_set.add(key)
 
     def insert(self, obj, key):
-        '''Insert the passed object into the cache, referenced by key,
+        """Insert the passed object into the cache, referenced by key,
         a hashable.
-        '''
+        """
         with self._cull_lock:
             self._cache_dict[key] = obj
             self._refresh(key)
 
     def lookup(self, key):
-        '''Lookup the object referenced by key and return it, refreshing
+        """Lookup the object referenced by key and return it, refreshing
         the cache at the same time.
-        '''
+        """
         self._refresh(key)
         return self._cache_dict[key]
