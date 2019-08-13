7679e2b35c4f2b8bd1e41aa182c0175c125f155d
Gregory Lee <grlee77@gmail.com>
2019-08-13 06:59:41 -0400

    merge dia PR here

--- cupyx/scipy/sparse/dia.py
+++ cupyx/scipy/sparse/dia.py
@@ -79,16 +79,6 @@ class dia_matrix(data._data_matrix):
         else:
             return dia_matrix((data, self.offsets), shape=self.shape)
 
-    def diagonal(self, k=0):
-        rows, cols = self.shape
-        if k <= -rows or k >= cols:
-            raise ValueError("k exceeds matrix dimensions")
-        idx, = cupy.nonzero(self.offsets == k)
-        first_col, last_col = max(0, k), min(rows + k, cols)
-        if idx.size == 0:
-            return cupy.zeros(last_col - first_col, dtype=self.data.dtype)
-        return self.data[idx[0], first_col:last_col]
-
     def get(self, stream=None):
         """Returns a copy of the array on host memory.
 
