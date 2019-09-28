6f09fbab5aae4dc8f32ddeb487b4867c0186ac1d
Gregory Lee <grlee77@gmail.com>
2019-09-28 00:12:33 -0400

    misc

--- cupy/linalg/norms.py
+++ cupy/linalg/norms.py
@@ -23,39 +23,30 @@ _l2_fast = cupy.core.create_reduction_func(
     ('?->d', 'e->e', 'f->f', 'd->d',
      ('F->f', ('my_norm(in0)', None, None, None)),
      ('D->d', ('my_norm(in0)', None, None, None))),
-    ('in0 * in0', 'a + b',
-     'out0 = sqrt(a)', None),
+    ('in0 * in0', 'a + b', 'out0 = sqrt(a)', None),
     preamble=_norm_preamble)
 
 _l1_fast = cupy.core.create_reduction_func(
     'l1_fast',
     ('?->d', 'e->e', 'f->f', 'd->d', 'F->f', 'D->d'),
-    ('abs(in0)', 'a + b',
-     'out0 = a', None))
-
-_nonzero_preamble = '''
-template <typename T> __device__ bool _nonzero(T x) { return x != 0; }
-__device__ bool _nonzero(const complex<float>& x) { return x.real() != 0 || x.imag() != 0; }
-__device__ bool _nonzero(const complex<double>& x) { return x.real() != 0 || x.imag() != 0; }
-'''
+    ('abs(in0)', 'a + b', 'out0 = a', None))
 
 _l0_fast = cupy.core.create_reduction_func(
     'l0_fast',
     ('?->d', 'e->e', 'f->f', 'd->d', 'F->f', 'D->d'),
-    ('_nonzero(in0)', 'a + b',
-     'out0 = a', None), preamble=_nonzero_preamble)
+    ('in0 != type_in0_raw(0)', 'a + b', 'out0 = a', None))
 
 _absmax_fast = cupy.core.create_reduction_func(
     'l0_fast',
     ('?->d', 'e->e', 'f->f', 'd->d', 'F->f', 'D->d'),
-    ('abs(in0)', 'max(a, b)',
-     'out0 = a', None))
+    ('abs(in0)', 'max(a, b)', 'out0 = a', None),
+    0)
 
 _absmin_fast = cupy.core.create_reduction_func(
     'l0_fast',
     ('?->d', 'e->e', 'f->f', 'd->d', 'F->f', 'D->d'),
-    ('abs(in0)', 'min(a, b)',
-     'out0 = a', None))
+    ('abs(in0)', 'min(a, b)', 'out0 = a', None),
+    '1.0/0.0')  # identity=infinity
 
 
 def norm(x, ord=None, axis=None, keepdims=False):
@@ -104,6 +95,7 @@ def norm(x, ord=None, axis=None, keepdims=False):
             return _absmax_fast(x, axis=axis, keepdims=keepdims)
         elif ord == -numpy.Inf:
             return _absmin_fast(x, axis=axis, keepdims=keepdims)
+            #return abs(x).min(axis=axis, keepdims=keepdims)
         elif ord == 0:
             # Zero norm
             return _l0_fast(x, axis=axis, keepdims=keepdims)
@@ -166,6 +158,133 @@ def norm(x, ord=None, axis=None, keepdims=False):
         return ret
     else:
         raise ValueError('Improper number of dimensions to norm.')
+
+def old_norm(x, ord=None, axis=None, keepdims=False):
+    """Returns one of matrix norms specified by ``ord`` parameter.
+
+    See numpy.linalg.norm for more detail.
+
+    Args:
+        x (cupy.ndarray): Array to take norm. If ``axis`` is None,
+            ``x`` must be 1-D or 2-D.
+        ord (non-zero int, inf, -inf, 'fro'): Norm type.
+        axis (int, 2-tuple of ints, None): 1-D or 2-D norm is cumputed over
+            ``axis``.
+        keepdims (bool): If this is set ``True``, the axes which are normed
+            over are left.
+
+    Returns:
+        cupy.ndarray
+
+    """
+    if not issubclass(x.dtype.type, numpy.inexact):
+        x = x.astype(float)
+
+    # Immediately handle some default, simple, fast, and common cases.
+    if axis is None:
+        ndim = x.ndim
+        if (ord is None or (ndim == 1 and ord == 2) or
+                (ndim == 2 and ord in ('f', 'fro'))):
+            if x.dtype.kind == 'c':
+                s = abs(x.ravel())
+                s *= s
+                ret = cupy.sqrt(s.sum())
+            else:
+                ret = cupy.sqrt((x * x).sum())
+            if keepdims:
+                ret = ret.reshape((1,) * ndim)
+            return ret
+
+    # Normalize the `axis` argument to a tuple.
+    nd = x.ndim
+    if axis is None:
+        axis = tuple(range(nd))
+    elif not isinstance(axis, tuple):
+        try:
+            axis = int(axis)
+        except Exception:
+            raise TypeError(
+                '\'axis\' must be None, an integer or a tuple of integers')
+        axis = (axis,)
+
+    if len(axis) == 1:
+        if ord == numpy.Inf:
+            return abs(x).max(axis=axis, keepdims=keepdims)
+        elif ord == -numpy.Inf:
+            return abs(x).min(axis=axis, keepdims=keepdims)
+        elif ord == 0:
+            # Zero norm
+            # Convert to Python float in accordance with NumPy
+            return (x != 0).astype(x.real.dtype).sum(
+                axis=axis, keepdims=keepdims)
+        elif ord == 1:
+            # special case for speedup
+            return abs(x).sum(axis=axis, keepdims=keepdims)
+        elif ord is None or ord == 2:
+            # special case for speedup
+            if x.dtype.kind == 'c':
+                s = abs(x)
+                s *= s
+            else:
+                s = x * x
+            return cupy.sqrt(s.sum(axis=axis, keepdims=keepdims))
+        else:
+            try:
+                float(ord)
+            except TypeError:
+                raise ValueError('Invalid norm order for vectors.')
+
+            absx = abs(x)
+            absx **= ord
+            ret = absx.sum(axis=axis, keepdims=keepdims)
+            ret **= cupy.reciprocal(ord, dtype=ret.dtype)
+            return ret
+    elif len(axis) == 2:
+        row_axis, col_axis = axis
+        if row_axis < 0:
+            row_axis += nd
+        if col_axis < 0:
+            col_axis += nd
+        if not (0 <= row_axis < nd and 0 <= col_axis < nd):
+            raise ValueError('Invalid axis %r for an array with shape %r' %
+                             (axis, x.shape))
+        if row_axis == col_axis:
+            raise ValueError('Duplicate axes given.')
+        if ord == 1:
+            if col_axis > row_axis:
+                col_axis -= 1
+            ret = abs(x).sum(axis=row_axis).max(axis=col_axis)
+        elif ord == numpy.Inf:
+            if row_axis > col_axis:
+                row_axis -= 1
+            ret = abs(x).sum(axis=col_axis).max(axis=row_axis)
+        elif ord == -1:
+            if col_axis > row_axis:
+                col_axis -= 1
+            ret = abs(x).sum(axis=row_axis).min(axis=col_axis)
+        elif ord == -numpy.Inf:
+            if row_axis > col_axis:
+                row_axis -= 1
+            ret = abs(x).sum(axis=col_axis).min(axis=row_axis)
+        elif ord in [None, 'fro', 'f']:
+            if x.dtype.kind == 'c':
+                s = abs(x)
+                s *= s
+                ret = cupy.sqrt(s.sum(axis=axis))
+            else:
+                ret = cupy.sqrt((x * x).sum(axis=axis))
+        else:
+            raise ValueError('Invalid norm order for matrices.')
+        if keepdims:
+            ret_shape = list(x.shape)
+            ret_shape[axis[0]] = 1
+            ret_shape[axis[1]] = 1
+            ret = ret.reshape(ret_shape)
+        return ret
+    else:
+        raise ValueError('Improper number of dimensions to norm.')
+
+
 
 
 # TODO(okuta): Implement cond
