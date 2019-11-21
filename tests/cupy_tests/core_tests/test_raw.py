3647cc956cf40711b5a1d78a3243a5d851e18c91
Leo Fang <leofang@bnl.gov>
2019-11-20 23:31:17 -0500

    Rename a variable

--- tests/cupy_tests/core_tests/test_raw.py
+++ tests/cupy_tests/core_tests/test_raw.py
@@ -387,11 +387,11 @@ class TestRaw(unittest.TestCase):
         ker = mod.get_function('multiply_by_const')
         mem_ptr = mod.get_global('some_array')
         const_arr = cupy.ndarray((100,), cupy.float32, mem_ptr)
-        const_data = cupy.arange(100, dtype=cupy.float32)
-        const_arr[...] = const_data
+        data = cupy.arange(100, dtype=cupy.float32)
+        const_arr[...] = data
         output_arr = cupy.ones(100, dtype=cupy.float32)
         ker((1,), (100,), (output_arr, cupy.int32(100)))
-        assert (const_data == output_arr).all()
+        assert (data == output_arr).all()
 
     def test_dynamical_parallelism(self):
         ker = cupy.RawKernel(_test_source4, 'test_kernel', options=('-dc',),
