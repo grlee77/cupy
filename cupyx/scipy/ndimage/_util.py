ba83b34772075617213ae03ab66080add0ece68e
Gregory Lee <grlee77@gmail.com>
2024-05-04 16:29:14 -0400

    support axes kwarg for gaussian_filter and uniform_filter
    * support per-axis modes for gaussian_filter and uniform_filter
    * support per-axis radius for gaussian_filter

--- cupyx/scipy/ndimage/_util.py
+++ cupyx/scipy/ndimage/_util.py
@@ -1,4 +1,6 @@
+import operator
 import warnings
+from collections.abc import Iterable
 
 import numpy
 
@@ -91,6 +93,25 @@ def _check_mode(mode):
         msg = f'boundary mode not supported (actual: {mode})'
         raise RuntimeError(msg)
     return mode
+
+
+def _check_axes(axes, ndim):
+    if axes is None:
+        return tuple(range(ndim))
+    elif cupy.isscalar(axes):
+        axes = (operator.index(axes),)
+    elif isinstance(axes, Iterable):
+        for ax in axes:
+            axes = tuple(operator.index(ax) for ax in axes)
+            if ax < -ndim or ax > ndim - 1:
+                raise ValueError(f"specified axis: {ax} is out of range")
+        axes = tuple(ax % ndim if ax < 0 else ax for ax in axes)
+    else:
+        message = "axes must be an integer, iterable of integers, or None"
+        raise ValueError(message)
+    if len(tuple(set(axes))) != len(axes):
+        raise ValueError("axes must be unique")
+    return axes
 
 
 def _get_inttype(input):
